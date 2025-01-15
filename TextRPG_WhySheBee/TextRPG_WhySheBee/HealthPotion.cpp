3aa01e929d790ecd42370e5abbbf0b18657bd3b7
jeongukchoi <jwchoi955@gmail.com>
2025-01-15 20:35:23 +0900

    fix_아이템 사용시 cout 삭제

--- TextRPG_WhySheBee/TextRPG_WhySheBee/HealthPotion.cpp
+++ TextRPG_WhySheBee/TextRPG_WhySheBee/HealthPotion.cpp
@@ -9,7 +9,6 @@ void HealthPotion::Use()
 {
 	PlayerCharacter* character = PlayerCharacter::GetPlayer();
 	character->IncreaseStat(TargetStat, StatAmount);
-	cout << GetTargetStatString() << "이 " << StatAmount << " 회복되었습니다.\n현재 체력 : " << character->GetHealth() << " / 최대 체력 : " << character->GetMaxHealth();
 }
 
 string HealthPotion::GetItemInfoString()
